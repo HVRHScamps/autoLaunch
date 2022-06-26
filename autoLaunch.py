cf84708893ff5d55680b327bbed5195f981766b5
cannotilever <51347140+cannotilever@users.noreply.github.com>
2022-06-26 10:58:51 -0400

    reset for week 2

--- autoLaunch.py
+++ autoLaunch.py
@@ -1,42 +1,9 @@
+# inclue code to help us talk to the robot
 import libhousy
-import time
-#You can define helper functions here, make sure to but them *above* the main function
-def launch(robot):
-    robot.beltZ1.Set(-0.8)
-    robot.beltZ2.Set(-0.8)
-    robot.beltZ3.Set(1)
-    robot.upperTension.Extend()
-    robot.lowerTension.Retract()
-    
-firstRun = True
-lastCount = 0
-ticks = 0
-launchTimer = 0
+
 def main(robot: libhousy.robot):
-    global firstRun, lastCount, ticks, launchTimer
-    if firstRun:
-        firstRun = False
-        launchTimer = time.time()
-    if robot.controller.getAxis(robot.controller.Axis.rTrigger) > 0.8:
-       robot.shootWheel.Set(1)
-
-    else:
-        robot.shootWheel.Set(0)
-
-    if time.time() - launchTimer >= 1:
-        launchTimer = time.time()
-        ticks = robot.shootCounter.Get() - lastCount
-        lastCount = robot.shootCounter.Get()
-
-    if ticks > 1800:
-        launch(robot)
-
-    else:
-        robot.beltZ1.Set(0)
-        robot.beltZ2.Set(0)
-        robot.beltZ3.Set(0)
-
-   
-
-       
-   
+    # Here is where your recurring code will go
+    print("Hello World!")
+    
+    # After everything is done, we tell the main program to stop us
+    return libhousy.
