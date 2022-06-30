07c7fe757a8e1b885dc41f0a4692abcb9c2c74b7
space-muffin <bbell09@icloud.com>
2022-06-30 09:47:19 -0400

    test #2

--- autoLaunch.py
+++ autoLaunch.py
@@ -5,11 +5,10 @@ import time
 start_time = time.time()
 last_count = 0
 def main(robot: libhousy.robot):
-    global start_time
+    global start_time, last_count
     # Here is where your recurring code will go
     if robot.controller.getAxis(robot.controller.Axis.rTrigger) >=.8:
         robot.shootWheel.Set(1)
-
     else:
         robot.shootWheel.Set(0)
         robot.pickupMotor.Set(0)
@@ -20,14 +19,13 @@ def main(robot: libhousy.robot):
     
     if time.time() - start_time > 0.5:
         start_time = time.time()
-
-        if robot.shootCounter.Get() - last_count  > 950:
-
+        if robot.shootCounter.Get() - last_count  > 900:
             robot.beltZ1.Set(-0.8)
             robot.beltZ2.Set(-0.8)
             robot.beltZ3.Set(1)
             robot.upperTension.Extend()
             robot.lowerTension.Retract()
+        last_count = robot.shootCounter.Get()
    
     
     # After everything is done, we tell the main program to stop us
